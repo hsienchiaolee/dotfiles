8c2143fbd9b49fbc5ec41ec2b7a38dfe5eda3415
Kai Lee <hlee027@gmail.com>
2025-11-22 22:38:17 -0800

    Clean up projectile setup

--- .emacs.d/settings/setup-projectile.el
+++ .emacs.d/settings/setup-projectile.el
@@ -11,7 +11,7 @@
   (setq projectile-use-git-grep t)
   (setq projectile-enable-caching t)
   :config
-  (projectile-global-mode t)
+  (projectile-mode +1)
 
   (defhydra hydra-projectile-other-window (:color teal)
     "projectile-other-window"
@@ -28,24 +28,23 @@
 
      Find File            Search/Tags          Buffers                Cache
 ------------------------------------------------------------------------------------------
-_s-f_: file            _a_: ag                _b_: switch to buffer  _c_: cache clear
- _ff_: file dwim       _g_: update gtags      _K_: Kill all buffers  _x_: remove known project
-  _r_: recent fie                                                ^^^^_X_: cleanup non-existing
-  _h_: helm                                                      ^^^^_z_: cache current
+_s-f_: file            _a_: rg                _b_: switch to buffer  _c_: cache clear
+ _ff_: file dwim       _h_: counsel           _K_: Kill all buffers  _x_: remove known project
+  _r_: recent file                                                ^^^^_X_: cleanup non-existing
+                                                                  ^^^^_z_: cache current
 "
-    ("a"   helm-projectile-rg)
-    ("b"   helm-projectile-switch-to-buffer)
+    ("a"   counsel-projectile-rg)
+    ("b"   counsel-projectile-switch-to-buffer)
     ("c"   projectile-invalidate-cache)
     ("h"   counsel-projectile)
     ("s-f" counsel-projectile-find-file)
     ("ff"  counsel-projectile-find-file-dwim)
-    ("g"   ggtags-update-tags)
     ("i"   projectile-ibuffer)
     ("K"   projectile-kill-buffers)
     ("s-p" projectile-switch-project "switch project")
     ("p"   projectile-switch-project)
     ("s"   projectile-switch-project)
-    ("r"   helm-projectile-recentf)
+    ("r"   projectile-recentf)
     ("x"   projectile-remove-known-project)
     ("X"   projectile-cleanup-known-projects)
     ("z"   projectile-cache-current-file)
@@ -53,9 +52,6 @@ _s-f_: file            _a_: ag                _b_: switch to buffer  _c_: cache 
     ("q"   nil "cancel" :color blue))
 
   (global-set-key (kbd "C-> o") 'hydra-projectile/body))
-
-(use-package helm-projectile
-  :ensure t)
 
 (use-package counsel-projectile
   :ensure t
