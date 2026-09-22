e70bc36f4c8b656aa259bd8ce9dde19f9d9f495f
Kai Lee <k@kaisky.co>
2026-09-21 20:59:07 -0700

    Refresh package archives before installation and suppress mode hooks

--- .emacs.d/settings/setup-package.el
+++ .emacs.d/settings/setup-package.el
@@ -7,15 +7,32 @@
 
 (require 'package)
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")
                          ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 (package-initialize)
 
+(defvar setup-package--refreshed-p nil
+  "Whether package archives have been refreshed for this session.")
+
+(defun setup-package--install (install &rest args)
+  "Refresh archives once before INSTALL with ARGS, without editing hooks."
+  ;; MELPA replaces old builds, so cached download URLs can stop working.
+  ;; Installed packages never call `package-install', keeping startup offline.
+  (unless setup-package--refreshed-p
+    (package-refresh-contents)
+    (setq setup-package--refreshed-p t))
+  ;; Autoload generation visits Lisp files before dependencies are activated.
+  ;; Programming hooks (such as Flycheck) must not run in those buffers.
+  (let ((prog-mode-hook nil)
+        (emacs-lisp-mode-hook nil))
+    (apply install args)))
+
+(advice-add 'package-install :around #'setup-package--install)
+
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
   (package-install 'use-package))
 (eval-when-compile
   (require 'use-package))
